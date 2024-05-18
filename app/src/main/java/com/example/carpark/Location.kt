ff5fa3f108f9b68dffa3358e4db434e60c8b0b51
Chudory Ashley <ashley.chudory@uninsubria.it>
2024-05-18 10:46:05 +0200

    Creazione classe Location

---
+++ app/src/main/java/com/example/carpark/Location.kt
@@ -0,0 +1,10 @@
+package com.example.carpark
+
+class Location(
+    val date: String = "",
+    val time: String = "",
+    val address: String = "",
+    val country: String = "",
+    val locality: String = "",
+    val latitude: Double = 0.0,
+    val longitude: Double = 0.0)
